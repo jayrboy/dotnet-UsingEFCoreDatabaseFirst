9406f8b66351f022f77424f49e985ffb75373259
jarboy <j.jayox_@hotmail.com>
2024-07-28 18:49:13 +0700

    Create Form and Post

--- UsingEFCoreDatabaseFirst/Controllers/ProductController.cs
+++ UsingEFCoreDatabaseFirst/Controllers/ProductController.cs
@@ -4,18 +4,50 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using UsingEFCoreDatabaseFirst.Data;
+using UsingEFCoreDatabaseFirst.Models.db;
 
 namespace UsingEFCoreDatabaseFirst.Controllers
 {
     public class ProductController : Controller
     {
-        private NorthwindContext _db = new NorthwindContext();
+        private NorthwindContext _db;
+
+        public ProductController(NorthwindContext db)
+        {
+            this._db = db;
+        }
 
         public IActionResult Index()
         {
-            // var products = from p in _db.Products select p;
-            var products = _db.Products.Include(p => p.Category); // join table
+            // var products = from p in context.Products select p;
+            var products = _db.Products
+                                  .Include(p => p.Category)  // join table
+                                  .Include(p => p.Supplier); // join table
+            return View(products.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_db.Categories, "CategoryId", "CategoryName");
+            ViewData["SupplierId"] = new SelectList(_db.Suppliers, "SupplierId", "CompanyName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product products)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(products);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CategoryId"] = new SelectList(_db.Categories, "CategoryId", "CategoryName");
+            ViewData["SupplierId"] = new SelectList(_db.Suppliers, "SupplierId", "CompanyName");
             return View(products);
         }
 
